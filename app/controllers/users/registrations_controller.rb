79398893dcf9e2afeeaaf5f1615248a03560a540
annieherieau <annie.herieau@gmail.com>
2024-06-06 15:56:31 +0200

    fix registration

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,29 +1,5 @@
 # frozen_string_literal: true
 
-# module Users
-#   class RegistrationsController < Devise::RegistrationsController # rubocop:todo Style/Documentation
-#     respond_to :json
-
-#     private
-
-#     def respond_with(resource, _opts = {})
-#       register_success && return if resource.persisted?
-
-#       register_failed
-#     end
-
-#     def register_success
-#       render json: {
-#         message: 'Signed up sucessfully.',
-#         user: current_user
-#       }, status: :ok
-#     end
-
-#     def register_failed
-#       render json: { message: 'Something went wrong.' }, status: :unprocessable_entity
-#     end
-#   end
-# end
 class Users::RegistrationsController < Devise::RegistrationsController
   respond_to :json
 
