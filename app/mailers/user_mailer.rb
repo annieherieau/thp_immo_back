3d0b71f314920be35df495528ef6ab899d4a009b
annieherieau <annie.herieau@gmail.com>
2024-06-07 20:53:23 +0200

    fix lien reset email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
   def reset_password_instructions(user, token)
     #je récupère l'instance user pour ensuite pouvoir la passer à la view en @user
     @user = user 
-    @url = application_url + '/edit?reset_password_token='+ token
+    @url = application_url + 'edit?reset_password_token='+ token
     @signin_url = application_url + "users/sign_in"
 
     #je permets d'envoyer l’e-mail en définissant le destinataire et le sujet.
