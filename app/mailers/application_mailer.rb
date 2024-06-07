5cb72129b5ed3902d10426c6d6f3497110dacb82
annieherieau <annie.herieau@gmail.com>
2024-06-07 10:05:14 +0200

    config mailjet

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base # rubocop:todo Style/Documentation
-  default from: 'from@example.com'
-  layout 'mailer'
+  default from: ENV["MAILJET_DEFAULT_FROM"]
+  layout "mailer"
+
+  def application_url
+    if Rails.env.production?
+      ENV['PROD_HOST']
+    else
+      ENV['DEV_HOST']
+    end
+  end
 end
