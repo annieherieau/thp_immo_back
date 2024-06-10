58e11a2da06941c98b15d9706acf0df6d04c88cd
annieherieau <annie.herieau@gmail.com>
2024-06-10 13:32:04 +0200

    affichage du profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,18 +1,28 @@
 # frozen_string_literal: true
 
 class ProfilesController < ApplicationController # rubocop:todo Style/Documentation
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
 
   def show
-    user = get_user_from_token
-    render json: {
-      message: "If you see this, you're in!",
-      user:
-    }
+    @user = get_user_from_token
+    if @user
+      render json: {
+        status: {code: 200,
+        message: "If you see this, you're in!"},
+        user: @user
+      }
+    else
+      render json: {
+        status: {code: 404,
+        message: "User Not Found"}
+      }
+    end
   end
 
   private
-
+  def get_user
+    
+  end
   def get_user_from_token # rubocop:todo Naming/AccessorMethodName
     jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1],
                              Rails.application.credentials.devise[:jwt_secret_key]).first
