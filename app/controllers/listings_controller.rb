6709ccddf49c31ef8ba03fead4e9c0e1b0f883c6
Korblen <malo.bastianelli@gmail.com>
2024-06-11 17:52:21 +0200

    images fonctionnelles

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -4,41 +4,27 @@ class ListingsController < ApplicationController
   before_action :set_user, only: %i[index_per_user]
   before_action :authorize_user!, only: %i[ update destroy ]
 
-
   # GET /listings
   def index
-    @listings = Listing.all.map
-    # @listings = Listing.all.map do |listing|
-    #   {listing: listing, user_email: listing.user.email}
-    # end
+    @listings = Listing.all.map { |listing| listing_with_photo_url(listing) }
     render json: @listings
   end
 
   # GET /cities/:city_id/listings
   def index_per_city
-    @filtered_listings = Listing.all.filter do |listing|
-      @city_id == listing.city_id
-    end
-    # @listings = @filtered_listings.map do |listing|
-    #   {listing: listing, user_email: listing.user.email}
-    # end
-    render json:  @filtered_listings
+    @filtered_listings = Listing.where(city_id: @city_id).map { |listing| listing_with_photo_url(listing) }
+    render json: @filtered_listings
   end
 
   # GET /users/:user_id/listings
   def index_per_user
-    @filtered_listings = Listing.all.filter do |listing|
-      @user_id === listing.user_id
-    end
-    # @listings = @filtered_listings.map do |listing|
-    #   {listing: listing, user_email: listing.user.email}
-    # end
-    render json: @filtered_listings
+    @listings = @user.listings.map { |listing| listing_with_photo_url(listing) }
+    render json: @listings
   end
 
   # GET /listings/1
   def show
-    render json: {listing: @listing, user_email: @listing.user.email}
+    render json: listing_with_photo_url(@listing)
   end
 
   # POST /listings
@@ -46,18 +32,17 @@ class ListingsController < ApplicationController
     @listing = Listing.new(listing_params)
     @listing.user = get_user_from_token
     if @listing.save
-      render json: @listing, status: :created
+      render json: listing_with_photo_url(@listing), status: :created
     else
-      puts @listing.errors.full_messages # Ajoutez cette ligne pour afficher les erreurs de validation
+      puts @listing.errors.full_messages # Display validation errors
       render json: @listing.errors, status: :unprocessable_entity
     end
   end
 
-
   # PATCH/PUT /listings/1
   def update
     if @listing.update(listing_params)
-      render json: @listing
+      render json: listing_with_photo_url(@listing)
     else
       render json: @listing.errors, status: :unprocessable_entity
     end
@@ -66,26 +51,27 @@ class ListingsController < ApplicationController
   # DELETE /listings/1
   def destroy
     if @listing.destroy!
-      render json: { message: "listing deleted"}
+      render json: { message: "listing deleted" }
     else
       render json: @listing.errors, status: :unprocessable_entity
     end
   end
 
   private
+
     # Use callbacks to share common setup or constraints between actions.
     def set_listing
       @listing = Listing.find(params[:id])
     end
 
     def set_city
-      @city_id = params[:city_id].to_i;
+      @city_id = params[:city_id].to_i
     end
 
     def set_user
-      @user_id = params[:user_id].to_i;
+      @user = User.find(params[:user_id].to_i)
     end
-    
+
     # Ensure the current user is the owner of the listing
     def authorize_user!
       head :forbidden unless @listing.user_id == current_user.id
@@ -97,9 +83,13 @@ class ListingsController < ApplicationController
     end
 
     def get_user_from_token
-      jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1],
-                               Rails.application.credentials.devise[:jwt_secret_key]).first
+      jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1], Rails.application.credentials.devise[:jwt_secret_key]).first
       user_id = jwt_payload['sub']
       User.find(user_id.to_s)
+    end
+
+    # Helper method to include the photo URL in the listing JSON
+    def listing_with_photo_url(listing)
+      listing.as_json.merge(photo_url: listing.photo.attached? ? url_for(listing.photo) : nil)
     end
 end
